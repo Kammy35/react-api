35f856c482b7a57f558346ee80d6af23c117b0a0
kamran nawaz <kammy007@hotmail.com>
2019-12-24 13:18:58 +0000

    update project

---
+++ build/precache-manifest.8df62465501f2e59e2efedfdee3df93b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641899bbb4498760478a064c6f32c732",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "17c0a3891acc8923b2a4",
+    "url": "/react-api/static/css/main.a5796646.chunk.css"
+  },
+  {
+    "revision": "62a77c8b65e64302ac2e",
+    "url": "/react-api/static/js/2.6d0a5bb5.chunk.js"
+  },
+  {
+    "revision": "17c0a3891acc8923b2a4",
+    "url": "/react-api/static/js/main.211142a1.chunk.js"
+  },
+  {
+    "revision": "01e43347ddfbf005b7b3",
+    "url": "/react-api/static/js/runtime-main.eba0c8f5.js"
+  }
+]);
